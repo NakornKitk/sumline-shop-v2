5468d94f53d149c1b48c6a6c988e6214da091374
Nakorn K <nakorn.kitk@gmail.com>
2024-08-06 22:13:59 +0700

    eleven commit

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,7 @@ import {persist} from 'zustand/middleware'
 let store = (set) => ({
         data: [],
         fetchData: async () => {
-            const response = await fetch('src/allproduct.json')
+            const response = await fetch('public/allproduct.json')
             const data = await response.json()
             set({ data: data });
         },
