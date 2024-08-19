bdbbc00b2b70667434a7d51612f723b355c5e226
Nakorn K <nakorn.kitk@gmail.com>
2024-08-20 02:31:03 +0700

    16th commit: adjust picture

--- src/components/home/ImageSlider.jsx
+++ src/components/home/ImageSlider.jsx
@@ -16,13 +16,13 @@ const ImageSlider = () => {
       <div className="absolute top-[47%] w-[100%] z-[90]">
         <div className="flex justify-between">
           <span
-            className="material-symbols-outlined text-[40px] cursor-pointer text-gray-500 hover:text-black"
+            className="material-symbols-outlined text-[40px] cursor-pointer text-black hover:text-red-500"
             onClick={prevSlide}
           >
             chevron_left
           </span>
           <span
-            className="material-symbols-outlined text-[40px] cursor-pointer text-gray-500 hover:text-black"
+            className="material-symbols-outlined text-[40px] cursor-pointer text-black hover:text-red-500"
             onClick={nextSlide}
           >
             chevron_right
@@ -35,7 +35,7 @@ const ImageSlider = () => {
         style={{ transform: `translateX(-${currentIndex * 50}%)` }}
       >
         <div className=" h-[460px] bg-[url('https://nakornkitk.github.io/sumline-shop-v2/images/bg1.jpg')] bg-cover bg-center w-[100%]">
-          <div className="h-[100%] py-[20px] flex items-start bg-[#F5F5F5] bg-opacity-60 pl-[175px]  ">
+          <div className="h-[100%] py-[20px] flex items-start pl-[175px]  ">
             <div className="my-auto w-[40%]">
               <p className="text-black text-3xl md:text-4xl font-light">
                 Looking for your favorite Yarn bag?
@@ -82,19 +82,19 @@ const ImageSlider = () => {
         </div>
 
         <div className=" h-[460px] bg-[#418B8A] bg-cover bg-center w-[100%]">
-          <div className="h-[100%] py-[20px] flex bg-[#F5F5F5] bg-opacity-30 backdrop-blur-lg 2xl:px-[175px] md:px-[50px] ">
-            <div className="pt-[20px] md:pt-[0px] w-[40%] mx-auto mt-[40px]">
+          <div className="h-[100%] py-[20px] flex  2xl:px-[175px] md:px-[50px] ">
+            <div className=" w-[40%] mx-auto ">
               <img
                 src="https://nakornkitk.github.io/sumline-shop-v2/images/bg2.jpg"
                 alt=""
-                className="md:h-[400px] mx-auto"
+                className="h-[440px] mx-auto"
               />
             </div>
             <div className="w-[60%] m-auto">
-              <p className="text-black text-2xl md:text-4xl font-light">
+              <p className="text-white text-2xl md:text-4xl font-light">
                 Our new product is coming soon...
               </p>
-              <p className="text-white text-1xl md:text-2xl font-light">
+              <p className="text-[#F0CD90] text-1xl md:text-2xl font-light">
                 dont forget to subscribe to get the latest new.
               </p>
               <div className="pt-[10px] flex">
